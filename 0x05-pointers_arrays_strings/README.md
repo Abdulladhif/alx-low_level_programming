6c94572d57602a7eca4f0225403510f2e7f36f6b
Abdulladhif <sabirbaari@gmail.com>
2022-09-19 02:09:18 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+0-main.c
+0-reset_to_98.c
+main.h
+README.md
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/*
+ * main: Header file containing prototypes for all functions
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+int _strlen(char *s);
+int _putchar(char);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif
