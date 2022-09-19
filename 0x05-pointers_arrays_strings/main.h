cede817212c6cb500361a67e73f9e508e4b655cc
Abdulladhif <sabirbaari@gmail.com>
2022-09-19 03:12:07 -0700

    task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts2(char *str);
+void _puts(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 int _strlen(char *s);
