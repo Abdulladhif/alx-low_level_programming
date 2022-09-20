e70a2eb9085c0de548d5b710c704f22f203ce357
Abdulladhif <sabirbaari@gmail.com>
2022-09-20 01:34:56 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies string
+ * @dest: destination
+ * @src: source
+ *
+ * Resturn: dest include terminator '\0'
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
