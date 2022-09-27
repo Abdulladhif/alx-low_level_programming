5f769d3f38c9e912e498e8cb05113ffcf7f9d55b
Abdulladhif <sabirbaari@gmail.com>
2022-09-27 05:34:06 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum the diognals of a square matrix of integer
+ *
+ * @a: This is the array to sum
+ * @size: This is the length of the array
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, diag1 = 0, diag2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		diag1 += *(a + i * (size + 1));
+		diag2 += *(a + i * (size - 1));
+	}
+
+	printf("%d, %d\n", diag1, diag2);
+}
