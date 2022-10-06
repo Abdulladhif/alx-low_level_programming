d313b90bf33cba187a97b102726807f291cdc301
Abdulladhif <sabirbaari@gmail.com>
2022-10-05 18:24:45 -0700

    task 1 and 2

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - loactes a substring
+ * @haystack: - string in which to check for needle
+ * @needle: - substring to find in haystack
+ *
+ * Return: a pointer to the beginning of the located substring, or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		j = 0;
+		while (needle[j])
+		{
+			if (needle[j] == haystack[i + j])
+				j++;
+			else if (needle[j] != haystack[i + j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+
+		i++;
+	}
+	return (NULL);
+}
+
