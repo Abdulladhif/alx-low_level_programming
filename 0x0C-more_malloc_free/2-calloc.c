3b68901c670fccc39c0fc320fa336fedafd0425d
Abdulladhif <sabirbaari@gmail.com>
2022-10-08 09:08:12 -0700

    mandatory

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * _calloc - Allocates memory for an array of a certain number
+ *           of elements each of an inputted byte size.
+ * @nmemb: The number of elements.
+ * @size: The byte size of each array element.
+ *
+ * Return: If nmemb = 0, size = 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int a, b;
+	char *call;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	b = (nmemb * size);
+	call = malloc(b);
+	if (call == NULL)
+		return (NULL);
+	for (a = 0; a < b; a++)
+	{
+		call[a] = 0;
+	}
+	return (call);
+}
+
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * array_range - function that creates an array of integers.
+ *
+ *
+ * @min: This is the range min to allocate in the memory
+ * @max: This is my range max to allocate in the memory
+ *
+ * Return: The pointer to the newly created array
+ *         If min > max, return NULL
+ *         If malloc fails, return NULL
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, index, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = min++;
+
+	return (array);
+}
+
